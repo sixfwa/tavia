cded8f456a1a575dfb0dc1f9086b10d6de3231d6
Francis <farhanawli6@gmail.com>
2019-10-24 20:18:53 +0200

    Consumer key and secret are not shown. Just the URLs are shown.

---
+++ constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = 'PRIVATE'
+CONSUMER_SECRET = 'PRIVATE'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
