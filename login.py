c54d8f025fa2c71659e77e5ad74e3e35f860f699
Francis <farhanawli6@gmail.com>
2019-10-31 20:48:06 +0100

    log in or signup

--- login.py
+++ login.py
@@ -2,46 +2,63 @@ import constants
 import oauth2
 import urllib.parse as urlparse
 import json
+from user import User
+from database import Database
 
 # Create a consumer, which uses CONSUMER_KEY and CONSUMER_SECRET to identify our app uniquely
-consumer = oauth2.Consumer(constants.CONSUMER_KEY, constants.CONSUMER_SECRET)
-client = oauth2.Client(consumer)
+consumer = oauth2.Consumer(
+    constants.CONSUMER_KEY, constants.CONSUMER_SECRET)
 
-# Use the client to perform a request for the request token
-response, content = client.request(constants.REQUEST_TOKEN_URL, 'POST')
+Database.initialise(user='tavia', password='1234',
+                    host='localhost', database='tavia')
 
-# Get the request token parsing the the query string returned
-if response.status != 200:
-    print("An error occured getting the request from Twitter")
+user_email = input("Enter your email-address: ")
+user = User.load_from_db_by_email(user_email)
 
-# The content is in the format of a query string parameter
-request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+if not user:
+    client = oauth2.Client(consumer)
 
-# Ask the user to authorize our app and give us the pin code
-print("Go to the following site in your browser: ")
-print("{}?oauth_token={}".format(
-    constants.AUTHORIZATION_URL, request_token['oauth_token']))
+    # Use the client to perform a request for the request token
+    response, content = client.request(constants.REQUEST_TOKEN_URL, 'POST')
 
-oauth_verifier = input("What is the PIN? ")
+    # Get the request token parsing the the query string returned
+    if response.status != 200:
+        print("An error occured getting the request from Twitter")
 
-# Create a Token object which contains the request token, and the verifier
-token = oauth2.Token(
-    request_token['oauth_token'], request_token['oauth_token_secret'])
-token.set_verifier(oauth_verifier)
+    # The content is in the format of a query string parameter
+    request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
 
-# Create a client with our consumer (our app) and the newly created (and verified) token
-client = oauth2.Client(consumer, token)
+    # Ask the user to authorize our app and give us the pin code
+    print("Go to the following site in your browser: ")
+    print("{}?oauth_token={}".format(
+        constants.AUTHORIZATION_URL, request_token['oauth_token']))
 
-# Ask Twitter for an access token, and Twitter knows it should give us it because we've verified the
-# request token
-response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
-access_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+    oauth_verifier = input("What is the PIN? ")
 
-print(access_token)
+    # Create a Token object which contains the request token, and the verifier
+    token = oauth2.Token(
+        request_token['oauth_token'], request_token['oauth_token_secret'])
+    token.set_verifier(oauth_verifier)
+
+    # Create a client with our consumer (our app) and the newly created (and verified) token
+    client = oauth2.Client(consumer, token)
+
+    # Ask Twitter for an access token, and Twitter knows it should give us it because we've verified the
+    # request token
+    response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
+    access_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+
+    print(access_token)
+
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+
+    user = User(user_email, first_name, last_name,
+                access_token['oauth_token'], access_token['oauth_token_secret'], None)
+    user.save_to_db()
 
 # Create an 'authorized_token' Token object and use that to perform Twitter API calls on behalf of the user
-authorized_token = oauth2.Token(
-    access_token['oauth_token'], access_token['oauth_token_secret'])
+authorized_token = oauth2.Token(user.oauth_token, user.oauth_token_secret)
 authorized_client = oauth2.Client(consumer, authorized_token)
 
 # Make Twitter API calls
