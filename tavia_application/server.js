cecdf460936031f53dd867567c0a21a909784b94
sixfwa <farhanaliw6@gmail.com>
2020-01-31 12:10:40 +0000

    routes have been defined

--- tavia_application/server.js
+++ tavia_application/server.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+// Connect Database
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false })); // Enables to get the data from req.body
+
 app.get("/", (req, res) => res.send("API Running"));
 
-const PORT = process.env.PORT || 5000;
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`server started on post ${PORT}`));
